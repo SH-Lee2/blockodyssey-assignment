2c6ea07e14a3af73087ef7061cc615fffaaf6dd7
이시형 <tlgud585@gmail.com>
2023-01-20 02:36:05 +0900

    refactor : select 컴포넌트 검색 조건, 페이지당 행 공용 컴포넌트로 수정

--- src/components/Select.jsx
+++ src/components/Select.jsx
@@ -1,9 +1,8 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./select.module.css";
 import { arrowDown, arrowUp } from "../assets";
-import { conditions } from "../constants";
 
-const SearchSelect = () => {
+const Select = ({ current, onOptionClick, options, direction, name }) => {
 	const [isExpand, setIsExpand] = useState(false);
 	const expandRef = useRef(null);
 
@@ -16,34 +15,54 @@ const SearchSelect = () => {
 		return () => document.removeEventListener("mousedown", handleClickOutside);
 	});
 
+	const selectHandler = (perPage) => {
+		onOptionClick(perPage);
+	};
+
 	return (
 		<div className={styles.wrapper} ref={expandRef}>
+			{name && <span>페이지당 행: </span>}
+
 			<div
 				className={styles.select}
 				onClick={() => setIsExpand((prev) => !prev)}
 			>
-				<span>전체</span>
-				{isExpand ? (
-					<img src={arrowDown} alt="expand icon" width={18} height={18} />
+				<span>{current}</span>
+				{!isExpand ? (
+					<img
+						src={direction ? arrowDown : arrowUp}
+						alt="expand icon"
+						width={18}
+						height={18}
+					/>
 				) : (
-					<img src={arrowUp} alt="expand icon" width={18} height={18} />
+					<img
+						src={direction ? arrowUp : arrowDown}
+						alt="expand icon"
+						width={18}
+						height={18}
+					/>
+				)}
+
+				{isExpand && (
+					<div
+						className={styles.expand}
+						style={direction === "top" ? { bottom: "48px" } : { top: "48px" }}
+					>
+						{options.map(({ value }) => (
+							<div
+								key={value}
+								className={styles.condition}
+								onClick={() => selectHandler(value)}
+							>
+								<span>{value}</span>
+							</div>
+						))}
+					</div>
 				)}
 			</div>
-			{isExpand && (
-				<div className={styles.expand}>
-					{conditions.map((condition) => (
-						<div
-							key={condition.name}
-							className={styles.condition}
-							onClick={() => setIsExpand(false)}
-						>
-							<span>{condition.name}</span>
-						</div>
-					))}
-				</div>
-			)}
 		</div>
 	);
 };
 
-export default SearchSelect;
+export default Select;
